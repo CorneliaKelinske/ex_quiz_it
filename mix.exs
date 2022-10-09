8ac38b9f11d9a983706c10e3d677cf132d6b2410
Alan Vardy <alan@vardy.cc>
2022-10-09 07:36:33 -0700

    Add tailwind

--- mix.exs
+++ mix.exs
@@ -64,6 +64,7 @@ defmodule ExQuizIt.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
 
       # tooling
       {:dialyxir, "~> 1.1", only: :test, runtime: false},
