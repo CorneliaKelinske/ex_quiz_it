bf20053c05e865198cb7df1a43c2409fc7bfe29e
Alan Vardy <alan@vardy.cc>
2022-10-10 15:30:07 -0700

    Add doctor to CI

--- lib/ex_quiz_it_web/live/quiz_live.ex
+++ lib/ex_quiz_it_web/live/quiz_live.ex
@@ -1,13 +1,18 @@
 defmodule ExQuizItWeb.QuizLive do
+  @moduledoc """
+  Main LiveView for the quiz
+  """
   use Phoenix.LiveView, layout: {ExQuizItWeb.LayoutView, "live.html"}
 
   @questions ["what does the fox say?", "what did the confident pickle say?", "are we there yet?"]
 
+  @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     question = Enum.random(@questions)
     {:ok, assign(socket, message: "Make a guess:", question: question, answer: "")}
   end
 
+  @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
 
@@ -25,6 +30,7 @@ defmodule ExQuizItWeb.QuizLive do
     """
   end
 
+  @impl Phoenix.LiveView
   def handle_event("guess", _value, socket) do
     answer = "No one knows"
     {:noreply, assign(socket, answer: answer)}
